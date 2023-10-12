e5521d5490e8f81f6448e5598c9bc3e93a90f19d
rindle-pogi <147171717+rindle-pogi@users.noreply.github.com>
2023-10-13 03:26:55 +0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full name of Ben?",
+    answer: "Benjamin Kirby Tennyson",
+    options: [
+      "Ben Tulfo",
+      "Benjamin Kirby Tennyson",
+      "Ben Tong",
+      "Ben Tennishong"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What alien can change reality?",
+    answer: "Alien X",
+    options: [
+      "Alien X",
+      "Grey Matter",
+      "Upgrade",
+      "Surge"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How Ben is related to Gwen?",
+    answer: "Ben's cousin",
+    options: [
+      "Ben's wife",
+      "Ben's ex",
+      "Ben's cousin",
+      "Ben's mother"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What do you call the watch that makes Ben transform to different forms of aliens?",
+    answer: "Omnitrix",
+    options: [
+      "G-Shock",
+      "Rolex",
+      "Analog watch",
+      "Omnitrix"
+    ]
+  },
+    {
+    numb: 5,
+    question: "With whom does Ben share his birthday?",
+    answer: "Gwen",
+    options: [
+      "Gwen",
+      "His Grandfather",
+      "Kevin",
+      "Vilgax"
+    ]
+  }
+
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+  //let person = prompt("Please enter your name", "Harry Potter");
+];
